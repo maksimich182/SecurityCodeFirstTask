5b93474f67574211a1606288fb12a61dbb0c1e4c
maksimich182 <kennypin@mail.ru>
2023-08-07 12:50:41 +0300

    FileManager icons. First run

--- app/src/main/java/com/example/securitycodefirsttask/MainFragment.kt
+++ app/src/main/java/com/example/securitycodefirsttask/MainFragment.kt
@@ -5,9 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainFragment : Fragment() {
 
+    var fileMenegerTestList = listOf<FileManagerElement>(
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_directory),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file),
+        FileManagerElement("asdfff", "asdfasdf", R.drawable.ic_file)
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,6 +42,13 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView: RecyclerView = view.findViewById(R.id.file_manager_list)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = FileManagerElementAdapter(fileMenegerTestList)
     }
 
 
